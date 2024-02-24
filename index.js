5c3ce5671be073bcb07a295ad1ff28674a30c304
Fawzi Gharib <fawz11gharib@gmail.com>
2024-02-24 04:10:10 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+let p = document.getElementById('text');
+console.log(p);
+document.addEventListener("DOMContentLoaded", function() {
+    p.textContent ='This is really cool!';
+  })
